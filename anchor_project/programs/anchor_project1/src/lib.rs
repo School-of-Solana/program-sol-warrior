94e6a658211c8cfc9b6b81df555edbb18ff4206e
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-11-15 22:01:58 +0530

    Add main program logic for the Anchor project, including initialization, deposit, and withdrawal functions for the vault.

---
+++ anchor_project/programs/anchor_project1/src/lib.rs
@@ -0,0 +1,26 @@
+#![allow(unexpected_cfgs)]
+use anchor_lang::prelude::*;
+
+mod errors;
+mod events;
+mod instructions;
+mod state;
+use instructions::*;
+declare_id!("Hi7PtFd6gyNVt7VWRHELpqU1FnaRG7sGu5S34WaJdFyJ");
+
+#[program]
+pub mod anchor_project {
+    use super::*;
+
+    pub fn init_vault(ctx: Context<InitVault>) -> Result<()> {
+        _init_vault(ctx)
+    }
+
+    pub fn deposit_tip(ctx: Context<DepositTip>, amount: u64) -> Result<()> {
+        _deposit_tip(ctx, amount)
+    }
+
+    pub fn withdraw_tip(ctx: Context<WithdrawTip>) -> Result<()> {
+        _withdraw_tip(ctx)
+    }
+}
