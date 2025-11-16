06f0ec008bc985de30898bf21d6322d46c5bcdef
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-11-16 23:06:30 +0530

    implement vault initialization tests.

--- anchor_project/tests/anchor_project.ts
+++ anchor_project/tests/anchor_project.ts
@@ -2,15 +2,55 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorProject } from "../target/types/anchor_project";
 
-describe("anchor_project", () => {
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import { assert } from "chai";
+import crypto from "crypto";
+
+describe("tipping_program", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.anchorProject1 as Program<AnchorProject>;
+  const provider = anchor.AnchorProvider.env();
+  const conn = provider.connection;
+  anchor.setProvider(provider);
+  const program = anchor.workspace.anchorProject as Program<AnchorProject>;
+  const creator = anchor.web3.Keypair.generate();
+  const alice = anchor.web3.Keypair.generate();
+  const charlie = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  before(async () => {
+    let sig = await provider.connection.requestAirdrop(
+      creator.publicKey,
+      1000000000
+    );
+    await provider.connection.requestAirdrop(alice.publicKey, 1000000000);
+    await conn.confirmTransaction(sig);
+    const creatorBal = await conn.getBalance(creator.publicKey);
+    console.log("Creators Pub: ", creator.publicKey.toBase58(), creatorBal);
+  });
+
+  describe("Initialize Vault", async () => {
+    it("Is initialized!", async () => {
+      const vaultPda = PublicKey.findProgramAddressSync(
+        [Buffer.from("vault"), creator.publicKey.toBuffer()],
+        program.programId
+      )[0];
+      const tx = await program.methods
+        .initVault()
+        .accounts({
+          creator: creator.publicKey,
+        })
+        .signers([creator])
+        .rpc();
+
+      const vaultAccount = await program.account.vault.fetch(vaultPda);
+      console.log("Vault Account", vaultAccount.creator.toBase58());
+      console.log("Vault Pda ", vaultPda.toBase58());
+      assert.equal(
+        vaultAccount.creator.toBase58(),
+        creator.publicKey.toBase58()
+      );
+      assert.equal(vaultAccount.totalTips.toNumber(), 0);
+      console.log("Your transaction signature", tx);
+    });
   });
 });
