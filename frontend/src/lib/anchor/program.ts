37fdd4a8395bdaa9afd9fedd505024d3317539cb
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-11-22 05:18:55 +0530

    Refactor getProgram function to accept connection and wallet as parameters, improving flexibility and error handling for wallet absence in the frontend.

--- frontend/src/lib/anchor/program.ts
+++ frontend/src/lib/anchor/program.ts
@@ -1,22 +1,30 @@
 import { Program, AnchorProvider, setProvider } from "@coral-xyz/anchor";
-import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
+import {
+  AnchorWallet,
+  useAnchorWallet,
+  useConnection,
+} from "@solana/wallet-adapter-react";
 import type { AnchorProject } from "./anchor_project_idl";
 import idl from "./anchor_project_idl.json";
+import { Connection } from "@solana/web3.js";
 
-export function getProgram() {
-  const { connection } = useConnection();
-  const wallet = useAnchorWallet();
+export function getProgram(
+  connection: Connection,
+  wallet: AnchorWallet
+): Program<AnchorProject> | null {
+  // const { connection } = useConnection();
 
-  if (!wallet) return null;
+  if (!wallet) {
+    console.error("No wallet found");
+    return null;
+  }
 
   const provider = new AnchorProvider(connection, wallet, {
     commitment: "confirmed",
   });
   setProvider(provider);
 
-  const program = new Program(idl as AnchorProject, {
-    connection,
-  });
+  const program = new Program(idl as AnchorProject, provider);
 
-  return program;
+  return program as unknown as Program<AnchorProject>;
 }
