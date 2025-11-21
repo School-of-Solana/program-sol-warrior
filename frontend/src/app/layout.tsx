c8f332bbb953d5f58322b42f27596b6b7170f8b9
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-11-22 05:17:25 +0530

    Update global styles to comment out background and text color, and modify layout background color for improved UI consistency in the frontend.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable}  antialiased bg-black text-white`}
+        className={`${geistSans.variable} ${geistMono.variable}  antialiased bg-zinc-50`}
       >
         <SolanaProvider>
           <BalanceProvider>{children}</BalanceProvider>
