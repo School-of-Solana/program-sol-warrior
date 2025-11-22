162b83d14c05313babeb20bf327a254b8162bc07
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-11-22 23:02:27 +0530

    Add tipping functionality to tip page, including wallet connection, deposit handling, and transaction status display, enhancing user experience and interaction in the frontend.

--- frontend/src/app/tip/[tipid]/page.tsx
+++ frontend/src/app/tip/[tipid]/page.tsx
@@ -2,23 +2,63 @@
 import LatestTxns from "@/src/components/LatestTxns";
 import { Button } from "@/src/components/ui/button";
 import { Input } from "@/src/components/ui/input";
+import { getDepositTip } from "@/src/lib/anchor/services";
+import { getConnection } from "@/src/lib/solana/connection";
 import { formatAddress } from "@/src/lib/utils";
+import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import { DM_Sans } from "next/font/google";
 import { useParams } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 const dmsans = DM_Sans({
   subsets: ["latin"],
 });
 
 const page = () => {
+  const wallet = useAnchorWallet();
+  const connection = getConnection();
   const { tipid } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [amount, setAmount] = useState<number>(0);
+  const [isSign, setIsSign] = useState("");
   console.log("Tip Id ", tipid);
 
   const builderAddForTip = tipid as string;
 
-  const handleTipWallet = () => {
+  const handleTipWallet = async () => {
     console.log("Handling tipping wallet");
+
+    try {
+      setIsLoading(true);
+
+      if (!wallet?.publicKey) {
+        console.log("Please connect your wallet.");
+        throw new Error("Please connect your wallet");
+      }
+
+      console.log("1", amount);
+      const sign = await getDepositTip(
+        wallet,
+        connection,
+        amount,
+        builderAddForTip
+      );
+
+      console.log(sign);
+
+      setIsSign(sign || "");
+    } catch (error) {
+      if (typeof error === "object" && error !== null && "message" in error) {
+        console.error(
+          "Deposit tip failed:",
+          (error as { message?: string }).message
+        );
+      } else {
+        console.error("Vault deposit tip failed:", error);
+      }
+    } finally {
+      setIsLoading(false);
+    }
   };
   return (
     <>
@@ -53,9 +93,12 @@ const page = () => {
           type="number"
           placeholder="0.001 SOL"
           className="text-blue-950  max-w-28"
+          min={0}
+          onChange={(e) => setAmount(Number(e.target.value))}
         />
         <Button
           variant={"secondary"}
+          disabled={isLoading}
           className="bg-[#522AA5] hover:bg-[#5128a5d9] text-white font-semibold cursor-pointer"
           onClick={handleTipWallet}
         >
@@ -64,6 +107,7 @@ const page = () => {
         </Button>
       </div>
 
+      {isSign && <p>https://explorer.solana.com/tx/{isSign}?cluster=devnet </p>}
       <div className="bg-zinc-50 h-full w-full">
         <h2 className="text-indigo-950">Latest Transactions:</h2>
         <LatestTxns />
