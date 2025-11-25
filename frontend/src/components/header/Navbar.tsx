b4899f59afd6d98e0a5e68e51e9f97691e601e50
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-11-25 17:00:59 +0530

    Update Navbar component to replace text with logo; integrate Image component for improved branding and visual appeal.

--- frontend/src/components/header/Navbar.tsx
+++ frontend/src/components/header/Navbar.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ConnectWallet from "./WalletConnect";
 import Link from "next/link";
+import Image from "next/image";
+import tiplogo from "@/src/images/tiplogo.png";
 
 const Navbar = () => {
   return (
     <div className="flex  items-center justify-between w-full">
       <Link href={"/"}>
-        <h1 className="text-2xl font-semibold text-[#000000ba]">Tipping</h1>
+        {/* <h1 className="text-2xl font-semibold text-[#000000ba]">Tipping</h1> */}
+
+        <div className="flex items-center justify-center w-10 ">
+          <Image alt="tip logo" src={tiplogo} width={60} />
+        </div>
       </Link>
       <ConnectWallet />
     </div>
